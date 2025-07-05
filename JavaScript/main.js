1fe4c333a7e2bdb44aac6be2206fe57dc4c5cb89
kms12425 <kms12425@qq.com>
2025-07-05 08:34:17 -0400

    增加几个project

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,3 +1,13 @@
-console.log("Hello, World!");
-var num = 1;
-console.log(num);
+var ourstr = "fsa";
+ourstr += ourstr;
+ourstr[0] = "j";
+console.log(ourstr);
+var result = abc(ourstr, ourstr)
+console.log(result)
+
+
+function abc(abv, diu)
+{
+    var result = abv + diu
+    return result;
+}
