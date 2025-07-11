d37f40b89c921900093b5c0fcf88f530feb73e12
kms12425 <kms12425@qq.com>
2025-07-11 16:15:53 -0400

    add a new file about python tutorial

---
+++ python/python tutorial/main.py
@@ -0,0 +1,12 @@
+mylist = list()
+print(mylist)
+mylist = [0] * 5
+print(mylist)
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+b = list1[2: 4]
+print(b)
+c = list(b)
+c = b.copy()
+b.append("f")
+print(c)
+print(b)
