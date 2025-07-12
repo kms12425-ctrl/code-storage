4eab9e3082529a0ba62a3342aef78c669a7726ff
kms12425 <kms12425@qq.com>
2025-07-12 15:36:19 -0400

    minor update

--- progect/blockchain/blockchain.py
+++ progect/blockchain/blockchain.py
@@ -2,3 +2,27 @@ class Blockchain (object):
     def __init__(self):
         self.chain = []
         self.current.transaction = []
+
+    def new_block(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+        self.current_transaction.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+        return self.last_block['index']+1
+
+    def new_transaction(self):
+        pass
+
+    def hash(block):
+        pass
+
+    def last_block(self):
+        pass
--- python/python tutorial/main.py
+++ python/python tutorial/main.py
@@ -1,12 +1,14 @@
-mylist = list()
-print(mylist)
-mylist = [0] * 5
-print(mylist)
-list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-b = list1[2: 4]
-print(b)
-c = list(b)
-c = b.copy()
-b.append("f")
-print(c)
-print(b)
+mydict = {"name": "ddd",
+          "age": 12,
+          "city": "New York"}
+value = mydict["age"]
+mydict.popitem()
+print(mydict)
+if ("djj" in mydict):
+    print(mydict["city"])
+elif ("name" in mydict):
+    print(mydict["name"])
+for key in mydict:
+    print(key)
+mydict["email"] = "ddd@hj.com"
+print(mydict)
