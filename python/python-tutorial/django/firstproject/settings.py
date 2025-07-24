169cdf32e5cb078f77dc45ac873b4aba5b026c9f
kms12425 <kms12425@qq.com>
2025-07-24 11:38:50 -0400

    update tutorial code

--- python/python-tutorial/django/firstproject/settings.py
+++ python/python-tutorial/django/firstproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x%=--yjv6sz*4wz=m3l@3*44!&r+1kfkf_h5&-qyyo4n9hq!nr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
