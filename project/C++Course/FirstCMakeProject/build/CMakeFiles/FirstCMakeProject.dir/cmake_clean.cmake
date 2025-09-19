5d9a33c2374e988605d7f953bce56344f67e4849
Ma Chun Ho <ma.rocky.oa@outlook.com>
2025-09-19 19:35:13 +0800

    update CMake tutorial code

---
+++ project/C++Course/FirstCMakeProject/build/CMakeFiles/FirstCMakeProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FirstCMakeProject.dir/main.cpp.obj"
+  "CMakeFiles/FirstCMakeProject.dir/main.cpp.obj.d"
+  "FirstCMakeProject.exe"
+  "FirstCMakeProject.exe.manifest"
+  "FirstCMakeProject.pdb"
+  "libFirstCMakeProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FirstCMakeProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
