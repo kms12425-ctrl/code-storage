ea29a4e8bab11925ba27a8c24df9746715a778d4
Ma Chun Ho <ma.rocky.oa@outlook.com>
2025-09-27 15:15:57 +0800

    update code

--- project/blockchain/blockchain.py
+++ project/blockchain/blockchain.py
@@ -1,9 +1,34 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain (object):
     def __init__(self):
         self.chain = []
-        self.current.transaction = []
+        self.current_transactions = []
+        self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self, sender, recipient, amount):
+    def new_block(self, proof, previous_hash=None):  # 如果previous_hash提供了参数就不是none
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+        block = {
+            'index': len(self.chain)+1,
+            'tiemstamp': time(),
+            'transaction': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
         """
         Creates a new transaction to go into the next mined Block
         :param sender: <str> Address of the Sender
@@ -11,18 +36,19 @@ class Blockchain (object):
         :param amount: <int> Amount
         :return: <int> The index of the Block that will hold this transaction
         """
-        self.current_transaction.append({
+        self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
-            'amount': amount
+            'amount': amount,
         })
-        return self.last_block['index']+1
+        return self.last_block['index'] + 1
 
-    def new_transaction(self):
-        pass
-
+    @staticmethod
     def hash(block):
+        # hashes a block
         pass
 
+    @property
     def last_block(self):
+        # return the last block in the chain
         pass
