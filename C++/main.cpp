668234e21945ce0a414299493b5587963f16da24
Ma Chun Ho <ma.rocky.oa@outlook.com>
2025-10-15 20:07:06 +0800

    update code

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-#include <algorithm>
-#include <cstring>
-#include <cctype>
-#include <vector>
-#include <cmath>
-#include <queue>
-#include <map>
-#include <stack>
-#include <unordered_set>
-#include <deque>
-#include <array>
-#include <list>
-#include <unordered_map>
-#include <windows.h>
-using namespace std;
-void dp(int n, vector<int> &dp);
+// #include <iostream>
+// #include <algorithm>
+// #include <cstring>
+// #include <cctype>
+// #include <vector>
+// #include <cmath>
+// #include <queue>
+// #include <map>
+// #include <stack>
+// #include <unordered_set>
+// #include <deque>
+// #include <array>
+// #include <list>
+// #include <unordered_map>
+// #include <windows.h>
+// using namespace std;
+// void dp(int n, vector<int> &dp);
 // int isprime(int n);
 // void swap(int *a, int *b);
 // int binary_low(int arr[], int n, int p);
@@ -30,34 +30,34 @@ void dp(int n, vector<int> &dp);
 //         return 1;
 //     return 0;
 // }
-int main()
-{
-    SetConsoleOutputCP(65001); // UTF-8 代码页
-    SetConsoleCP(65001);
-    printf("你好");
-    int n;
-    cin >> n;
-    vector<int> dp_arr;
-    dp_arr.push_back(1);
-    for (int i = 1; i <= n; i++)
-    {
-        dp(n, dp_arr);
-    }
-    cout << dp_arr[n];
-    return 0;
-}
-void dp(int n, vector<int> &dp)
-{
-    for (int i = 1; i <= n; i++)
-    {
-        if (i == 1)
-            return;
-        else
-        {
-            return;
-        }
-    }
-}
+// int main()
+// {
+//     SetConsoleOutputCP(65001); // UTF-8 代码页
+//     SetConsoleCP(65001);
+//     printf("你好");
+//     int n;
+//     cin >> n;
+//     vector<int> dp_arr;
+//     dp_arr.push_back(1);
+//     for (int i = 1; i <= n; i++)
+//     {
+//         dp(n, dp_arr);
+//     }
+//     cout << dp_arr[n];
+//     return 0;
+// }
+// void dp(int n, vector<int> &dp)
+// {
+//     for (int i = 1; i <= n; i++)
+//     {
+//         if (i == 1)
+//             return;
+//         else
+//         {
+//             return;
+//         }
+//     }
+// }
 // void swap(int *a, int *b)
 // {
 //     int temp = *a;
--- project/blockchain/blockchain.py
+++ project/blockchain/blockchain.py
@@ -1,13 +1,17 @@
 import hashlib
 import json
 from time import time
+from hashlib import sha256
+from uuid import uuid4
+
+from flask import Flask, jsonify
 
 
 class Blockchain (object):
     def __init__(self):
-        self.chain = []
-        self.current_transactions = []
-        self.new_block(previous_hash=1, proof=100)
+        self.chain = []  # 所有区块存放的地方
+        self.current_transactions = []  # 当前交易池
+        self.new_block(previous_hash=1, proof=100)  # 创世区块
 
     def new_block(self, proof, previous_hash=None):  # 如果previous_hash提供了参数就不是none
         """
@@ -28,7 +32,7 @@ class Blockchain (object):
         self.chain.append(block)
         return block
 
-    def new_transaction(self, sender, recipient, amount):
+    def new_transaction(self, sender, recipient, amount):  # 建立新的转账
         """
         Creates a new transaction to go into the next mined Block
         :param sender: <str> Address of the Sender
@@ -44,11 +48,72 @@ class Blockchain (object):
         return self.last_block['index'] + 1
 
     @staticmethod
-    def hash(block):
-        # hashes a block
-        pass
+    def hash(block):  # 建立哈希
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
-        # return the last block in the chain
-        pass
+        return self.chain[-1]
+
+    def proof_of_work(self, last_proof):  # 计算符合条件的proof
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+        proof = 0
+        while self.valid_proof(last_proof, proof) == False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):  # 判断last_proof + proof哈希值前4位是不是0000
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+
+# Instantiate our Node
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+node_identifier = str(uuid4()).replace('-', '')
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return 'We will mine a new block'
+
+
+@app.route('/transcations/new', methods=['POST'])
+def new_transaction():
+    return 'We will add a new transaction'
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+
+if (__name__ == '__main__'):
+    app.run(host='0.0.0.0', port=5000)
